3d7e2d0f552f28cfabfc875182ba5cd2c0fe151a
buhanzaz <Buhanza@gmail.com>
2025-12-15 02:14:04 +0300

    feat: add client configuration classes for EstimateDraft, ImageStorage, and LLM services

---
+++ services/ui-service/src/main/java/dev/buhanzaz/web/config/ImageStorageClientConfig.java
@@ -0,0 +1,26 @@
+package dev.buhanzaz.web.config;
+
+import dev.buhanzaz.web.service.ImageStorageClient;
+import dev.buhanzaz.web.service.LLMClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class ImageStorageClientConfig {
+
+    @Bean
+    public ImageStorageClient llmService() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl("http://localhost:8083") // URL сервиса с LLM
+                .build();
+
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+                .builderFor(RestClientAdapter.create(restClient))
+                .build();
+
+        return factory.createClient(ImageStorageClient.class);
+    }
+}
---
+++ services/ui-service/src/main/java/dev/buhanzaz/web/config/LLMClientConfig.java
@@ -0,0 +1,25 @@
+package dev.buhanzaz.web.config;
+
+import dev.buhanzaz.web.service.LLMClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class LLMClientConfig {
+
+    @Bean
+    public LLMClient llmService() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl("http://localhost:8081") // URL сервиса с LLM
+                .build();
+
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+                .builderFor(RestClientAdapter.create(restClient))
+                .build();
+
+        return factory.createClient(LLMClient.class);
+    }
+}
